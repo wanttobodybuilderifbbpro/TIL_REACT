79d46f4c8805dc952407f46a13faac8b4e61bc6e
박재형 <wogud114@gmail.com>
2022-12-20 18:20:12 +0900

    react

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = 'Gangnam Udong Restaurant';
+  // document.querySelector('h4').innerHTML = post; 생javascript에서의 방법
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          josh <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav">
+      <h4 style={{color : 'red', fontSize : '16px'}}>This is blog</h4>
+     </div>
+     <h4>{ post }</h4> 
     </div>
   );
 }
